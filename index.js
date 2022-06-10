6340ad1c1af40a71a3c8ea3c5de48b785e66840c
Vladislav <vladislav890@gmail.com>
2022-06-10 23:24:55 +0300

    without error

--- index.js
+++ index.js
@@ -1,41 +1,29 @@
+"use strict";
 Object.defineProperty(window, "MySweetApp", {
-  value: "v1.0.0",
-  readonly: false,
-})
-
+    value: "v1.0.0",
+    writable: true,
+});
 function deliveryMethod() {
-  // TODO make some calculation
-  return "overnight"
+    // TODO make some calculation
+    return "overnight";
 }
-
 function shipWeight() {
-  return parseInt(document.getElementById("weight").innerHTML)
+    return parseInt(document.getElementById("weight").innerHTML);
 }
-
-/**
- *
- * @param {(string | string[])} emailAddr
- */
-
 function sendUpdates(emailAddr) {
-  // string or string array
-
-  function sendEmail(addr) {
-    console.log(
-      `Shipping to ${addr} via ${deliveryMethod() | "standard"} delivery`,
-    )
-    if (shipWeight() > 100) {
-      console.log("WARNING: Oversize package")
+    // string or string array
+    function sendEmail(addr) {
+        console.log(`Shipping to ${addr} via ${deliveryMethod() || "standard"} delivery`);
+        if (shipWeight() > 100) {
+            console.log("WARNING: Oversize package");
+        }
     }
-  }
-
-  if (emailAddr.length) {
-    emailAddr.forEach((idx, val) => {
-      sendEmail(val.trim())
-    })
-  } else {
-    sendEmail(emailAddr.trim())
-  }
+    if (Array.isArray(emailAddr)) {
+        emailAddr.forEach((val) => {
+            sendEmail(val.trim());
+        });
+    }
+    else {
+        sendEmail(emailAddr.trim());
+    }
 }
-
-sendUpdates(1)
