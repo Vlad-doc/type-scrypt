892b1bf386658394d04857b3a00f16b79c843dcb
Your Name <you@example.com>
2022-06-10 14:06:33 +0300

    init app\

---
+++ index.js
@@ -0,0 +1,35 @@
+Object.defineProperty(window, "MySweetApp", {
+  value: "v1.0.0",
+  readonly: false,
+})
+
+function deliveryMethod() {
+  // TODO make some calculation
+  return "overnight"
+}
+
+function shipWeight() {
+  return parseInt(
+    document.getElementById("weight") /* @type {HTMLDivElement} */.innerHTML,
+  )
+}
+
+function sendUpdates(emailAddr) {
+  // string or string array
+  function sendEmail(addr) {
+    console.log(
+      `Shipping to ${addr} via ${deliveryMethod() | "standard"} delivery`,
+    )
+    if (shipWeight() > 100) {
+      console.log("WARNING: Oversize package")
+    }
+  }
+
+  if (emailAddr.length) {
+    emailAddr.forEach((idx, val) => {
+      sendEmail(val.trim())
+    })
+  } else {
+    sendEmail(emailAddr.trim())
+  }
+}
