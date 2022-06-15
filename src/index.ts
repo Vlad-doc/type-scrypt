6c6ee4b2944a88b8d83a803ef3f375cccdac8f1a
Vladislav <vladislav890@gmail.com>
2022-06-15 16:00:11 +0300

    union intersection

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,34 @@
 /* eslint-disable no-unused-vars */
 
-const user: {
-  readonly firstName: string
-  readonly age?: number // ? - необязательное поле
-} = {
-  firstName: "Igor",
+const sn: string | number = 22
+const user: { name: string } & { age: number } = { name: "Igor", age: 45 }
+
+interface IStudent {
+  id: string
+  name: string
+  lectures: string[]
 }
-user.age = 33
-
-const booleanMap: {
-  [userName: string]: typeof user
-} = {
-  Igor: {
-    firstName: "Igor",
-  },
+interface ITeacher {
+  id: number
+  name: string
+  lessons: string[]
+}
+function getSubjects(person: ITeacher | IStudent) {
+  if ("lessons" in person) {
+    return person.lessons
+  }
+  return person.lectures
 }
 
-const arr: Array<number> = []
-const arr1: number[] = []
-const arr2: number[][] = [
-  [1, 2, 3],
-  [4, 5, 6],
-]
-// arr2 && arr3 эквивалентные записи
-const arr3: typeof arr[] = [
-  [1, 2, 3],
-  [4, 5, 6],
-]
-const arr4: ReadonlyArray<number> = [1, 2, 3]
-arr4[50] = 34
-arr4.push(12)
-
-const arr5: [number, string, typeof user] = [1, "one", { firstName: "Igor" }]
-arr5.push(3) // баг
-
-const arr6 = [1, 2, 3] as const // аналогично ReadonlyArray<number>
+type worker = {
+  id: string
+  salary?: number
+  info: {
+    male: boolean
+  }
+}
+type toff = {
+  id: number
+  name: string
+  skill: []
+} & worker
