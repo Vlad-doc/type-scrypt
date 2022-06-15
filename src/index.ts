520cf1eeaa6cfdcd83ee9fbf94d29ef29661ca1a
Vladislav <vladislav890@gmail.com>
2022-06-15 12:31:20 +0300

    special types

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,21 @@
 /* eslint-disable no-unused-vars */
 
-type Digit = 1 | 2 | 3 | 4 | 5
-const num: Digit = 5
-const bum: Digit = 56
+const anyType: any = null
+anyType.a = 78
+anyType["a"] = "ghj"
+anyType()
 
-const sum: number = 34
+let objType: Object = { a: 1 }
+objType.a = 1
+objType = {}
+objType()
+objType = 1
+Object.create(objType)
 
-const bool: boolean = false
+let unkType: unknown = null
+unkType.a = 78
+unkType["a"] = "ghj"
+unkType = null
 
-enum Actions {
-  Top = 0,
-  Left = 90,
-}
-const direction: keyof typeof Actions = "Top"
-
-interface IFact {
-  factId: number
-  userForm: string
-  userRoad: string
-}
-const uniqueValue = (): "factId" | "userForm" | "userRoad" => {
-  return "factId"
-}
-const dataList: { action: string; data: IFact }[] = []
-dataList.map((item: { action: string; data: IFact }) => {
-  if (item.data[uniqueValue()] === 2) {
-    return 2
-  }
-  return item
-})
-
-const person = {
-  name: "Igor",
-  age: 33,
-  info: {
-    male: true,
-  },
-}
-type Person = typeof person
-const keys: keyof typeof person = "name"
-const values: Person[keyof Person] = { a: 1 }
+let voidType: void = undefined // undefined || null
+voidType = undefined
