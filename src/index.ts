64181bcb411132ddbe236ed986bc74866dd8d932
Vladislav <vladislav890@gmail.com>
2022-06-17 15:01:46 +0300

    this in function

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,9 @@
-function average(a: number, b: number, c: number): string {
-  const avg: number = (a + b + c) / 3
-  return `Average is ${avg}`
+function getFullName(this: { name: string; surename: string }) {
+  return `${this.name} ${this.surename}`
 }
-average(1) // An argument for 'b' was not provided.
-average(1, 2, "fg") // Argument of type 'string' is not assignable to parameter of type 'number'
-
-function getAverage(a: number, b?: number, c?: number): string {
-  if (b === undefined) {
-    b = 0
-  }
-  if (c === undefined) {
-    c = 0
-  }
-  const avg: number = (a + b + c) / 3
-  return `Average is ${avg}`
+const account = {
+  name: "Igor",
+  surename: "Fgh",
+  getFullName: getFullName,
 }
-getAverage(1)
-getAverage(1, 2)
-
-function argsAverage(...args: (string | number)[]): string {
-  let total: number = 0
-  for (const item of args) {
-    if (typeof item === "string") {
-      total += Number(item)
-      continue
-    }
-    total += item
-  }
-  const avg: number = total / args.length
-  return `${avg}`
-}
-argsAverage(2, 4, 5, 6, 7, 8)
+account.getFullName()
